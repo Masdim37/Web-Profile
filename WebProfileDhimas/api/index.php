bc777087e2b3073a1752af78388bd6f1584c1179
Masdim37 <dhimas.hfzh375@gmail.com>
2026-05-14 16:36:44 +0700

    fixing

--- WebProfileDhimas/api/index.php
+++ WebProfileDhimas/api/index.php
@@ -1,13 +1,2 @@
 <?php
-
-try {
-    // Memanggil entry point bawaan Laravel
-    require __DIR__ . '/../public/index.php';
-} catch (\Throwable $e) {
-    // Menangkap error sebelum Laravel mencoba merender tampilan (view)
-    echo "<h1 style='color:red;'>Error Asli Ditemukan!</h1>";
-    echo "<b>Pesan Error:</b> " . $e->getMessage() . "<br><br>";
-    echo "<b>Terjadi di File:</b> " . $e->getFile() . " (Baris " . $e->getLine() . ")<br><br>";
-    echo "<b>Stack Trace:</b><br>";
-    echo "<textarea style='width:100%; height:400px; background:#f4f4f4;'>" . $e->getTraceAsString() . "</textarea>";
-}
+require __DIR__ . '/../public/index.php';
